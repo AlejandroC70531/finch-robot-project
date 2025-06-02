3e591a210895aafb4edd928f4f8a0f0965291373
AlejandroC70531 <achavezjr0017@mymail.lausd.net>
2025-06-02 15:30:47 -0700

    Commit 06/02

--- src/main/java/com/alejandro/Test.java
+++ src/main/java/com/alejandro/Test.java
@@ -1,3 +1,24 @@
+package com.alejandro;
+
+import com.birdbrain.Finch;
+
 public class Test{
-    
+    public static void main(String[] args) {
+    Finch fitzgerald = new Finch();
+    System.out.println(fitzgerald.getDistance());
+    fitzgerald.setMove("F", 20, 70);
+    System.out.println(fitzgerald.getDistance());
+    fitzgerald.setMove("B", 5, 70);
+    System.out.println(fitzgerald.getDistance());
+    fitzgerald.navigate();
+    public void navigate(){
+      int distance = getDistance();
+      while(distance > 10){
+        fitzgerald.setMove("F", 5, 70);
+        distance = fitzgerald.getDistance();
+      }
+    }
+    fitzgerald.stopAll();
+    fitzgerald.disconnect();
+  }
 }
